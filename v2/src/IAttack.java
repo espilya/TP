0026a7839aaac3f84d6efac9a8d673e63206ca0a
RO8ER7O <55734556+RO8ER7O@users.noreply.github.com>
2019-12-02 20:39:16 +0100

    Delete IAttack.java

--- v2/src/IAttack.java
+++
@@ -1,6 +0,0 @@
-public interface IAttack {
-	default boolean performAttack(GameObject other) {return false;};
-	default boolean receiveMissileAttack(int damage) {return false;};
-	default boolean receiveBombAttack(int damage) {return false;};
-	default boolean receiveShockWaveAttack(int damage) {return false;};
-}
